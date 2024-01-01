0b8232c0534ae5fb6c91b8f1e7b1e34a56e098b3
BatuSevinc <batusvnc1@gmail.com>
2024-01-01 20:26:46 +0300

    added images.

--- src/components/ProductsFilter.jsx
+++ src/components/ProductsFilter.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ProductsFilter = () => {
   return (
     <div className='flex text-sm justify-center gap-4 flex-wrap'>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>çok satanlar</h6>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>tüm Kategoriler</h6>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>erkek botlar</h6>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>erkek klasik</h6>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>rengarenk</h6>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>kadın klasik</h6>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>kadın spor</h6>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>özenle tasarlanmış</h6>
-      <h6 className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>erkek spor</h6>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>çok satanlar</Link>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>tüm Kategoriler</Link>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>erkek botlar</Link>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>erkek klasik</Link>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>rengarenk</Link>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>kadın klasik</Link>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>kadın spor</Link>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>özenle tasarlanmış</Link>
+      <Link className='uppercase text-center font-rubik-lines transition-all duration-300 rounded-sm border-2 border-[#797777] p-1 hover:bg-[#797777] hover:text-white cursor-pointer'>erkek spor</Link>
     </div>
   )
 }
